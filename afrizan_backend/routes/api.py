7455231bab652e1a2adb9186f9011eb4bc554776
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-02-19 10:59:08 -0800

    updated routes.api

--- afrizan_backend/routes/api.py
+++ afrizan_backend/routes/api.py
@@ -1,21 +1,36 @@
 from flask_restful import Resource
-from models import db, User, Product, Order
+from models import db, User, Product, Order, Review, Category, Artisan
 
 class UserResource(Resource):
     def get(self):
         users = User.query.all()
-        return [user.__dict__ for user in users], 200
+        return [user.serialize() for user in users], 200
 
-    # Implement other HTTP methods as needed (POST, PUT, DELETE)
 
 class ProductResource(Resource):
     def get(self):
         products = Product.query.all()
-        return [product.__dict__ for product in products], 200
+        return [product.serialize() for product in products], 200
 
-    # Implement other HTTP methods as needed (POST, PUT, DELETE)
 
 class OrderResource(Resource):
     def get(self):
         orders = Order.query.all()
-        return [order.__dict__ for order in orders], 200
+        return [order.serialize() for order in orders], 200
+
+
+class ReviewResource(Resource):
+    def get(self):
+        reviews = Review.query.all()
+        return [review.serialize() for review in reviews], 200
+
+
+class CategoryResource(Resource):
+    def get(self):
+        categories = Category.query.all()
+        return [category.serialize() for category in categories], 200
+
+class ArtisanResource(Resource):
+    def get(self):
+        artisans = Artisan.query.all()
+        return [artisan.serialize() for artisan in artisans], 200
